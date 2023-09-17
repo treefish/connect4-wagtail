89e90fd3cdd2af0966d4c6344b9e5944c471d6f0
David Apimerika <David.Apimerika@treefish.co.nz>
2023-09-18 10:50:50 +1200

    Complete 02 - Installing Wagtail

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rocketman
 Learn Wagtail
+
+# Setup
+
+    git clone git@github.com:treefish/rocketman.git
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install wagtail
+    pip show wagtail
+    wagtail start rocketman .
+    python manage.py runserver 0.0.0.0:8013
+    
+    
+    
+    
---
+++ rocketman/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-47u=-n7koe4tr0#f7y_$o8*g%qs54o(52k3h&7zn-*nh48$joy"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
