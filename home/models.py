da314d4ea869a7c17023481bc26f7f582c3c8020
David Apimerika <David.Apimerika@treefish.co.nz>
2023-09-19 10:45:40 +1200

    Complete 06 - Editing the Home Page.

--- home/models.py
+++ home/models.py
@@ -1,7 +1,40 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
-    pass
+    lead_text = models.CharField(
+        max_length=140,
+        blank=True,
+        help_text="Subheading under the banner title",
+    )
+    button = models.ForeignKey(
+        "wagtailcore.Page",
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="Select an optional page to link to",
+        on_delete=models.SET_NULL,
+    )
+    button_text = models.CharField(
+        max_length=50,
+        default="Read More",
+        blank=False,
+        help_text="Button text",
+    )
+    banner_background_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=False,
+        null=True,
+        related_name="+",
+        help_text="The banner background image.",
+        on_delete=models.SET_NULL,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("lead_text"),
+        FieldPanel("button"),
+        FieldPanel("button_text"),
+        FieldPanel("banner_background_image")
+    ]
--- rocketman/settings/base.py
+++ rocketman/settings/base.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-nz"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "NZ"
 
 USE_I18N = True
 
