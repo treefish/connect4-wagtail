cc813f4af31e0d0b09ec8ab3f60fe917cdbef09a
David Apimerika <David.Apimerika@treefish.co.nz>
2023-09-21 10:52:40 +1200

    Complete 20 - Simplifying Repeating StreamField Blocks.

--- streams/blocks.py
+++ streams/blocks.py
@@ -3,11 +3,10 @@ from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleBlock(blocks.StructBlock):
-
-
-    text = blocks.CharBlock(required = True,
-                            help_text = "Text to display",
-                            )
+    text = blocks.CharBlock(
+        required=True,
+        help_text="Text to display",
+    )
 
     class Meta:
         template = "streams/title_block.html"
@@ -16,18 +15,33 @@ class TitleBlock(blocks.StructBlock):
         help_text = "Centered text to display on the page."
 
 
-class CardsBlock(blocks.StructBlock):
+class Link(blocks.StructBlock):
 
+    link_text = blocks.CharBlock(max_length=50, default="More Detail")
+    internal_page = blocks.PageChooserBlock(required=False)
+    external_link = blocks.URLBlock(required=False)
+
+
+class Card(blocks.StructBlock):
+    title = blocks.CharBlock(
+        max_length=100,
+        help_text="Bold title text for this card. Max length of 100 characters.",
+    )
+    text = blocks.TextBlock(
+        max_length=255,
+        help_text="Optional text for this card. Max length of 255 characters.",
+        required=False,
+    )
+    image = ImageChooserBlock(
+        help_text="Image will be automagically cropped 570px by 370px"
+    )
+    link = Link(help_text="Enter a URL link or select a page")
+
+
+
+class CardsBlock(blocks.StructBlock):
     cards = blocks.ListBlock(
-        blocks.StructBlock(
-            [
-                ("title", blocks.CharBlock(max_length=100,
-                help_text="Bold title text for this card. Max length of 100 characters.")),
-                ("text", blocks.TextBlock(max_length=255,
-                                           help_text="Optional text for this card. Max length of 255 characters.", required=False)),
-                ("image", ImageChooserBlock(help_text="Image will be automagically cropped 570px by 370px")),
-            ]
-        )
+        Card()
     )
 
     class Meta:
