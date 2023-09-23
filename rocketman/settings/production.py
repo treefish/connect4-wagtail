5c72f484370e40c25efe753e1e9b2b0a21c0b069
David Apimerika <David.Apimerika@treefish.co.nz>
2023-09-23 14:12:25 +1200

    Complete 43 - Preparing for Launch.

--- rocketman/settings/production.py
+++ rocketman/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 import os
 
 DEBUG = False
+ALLOWED_HOSTS = ["rocketman.treefish.co.nz", "localhost", "0.0.0.0", "127.0.0.1", "192.168.20.24"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 cwd = os.getcwd()
 CACHES = {
@@ -10,6 +12,53 @@ CACHES = {
         "LOCATION": f"{cwd}/.cache"
     }
 }
+
+# Comment this out when actually Production
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
+# Database PostgreSQL
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE"),
+        "NAME": os.environ.get("SQL_DATABASE"),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
+    }
+}
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://daa27f19110a886d29fc24eb461ba313@o4505927324073984.ingest.sentry.io/4505927335018496",
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 
 try:
     from .local import *
