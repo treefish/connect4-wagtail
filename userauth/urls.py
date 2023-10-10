814f4cda311dba2bfcb8011fd463e3893dff4f32
David Apimerika <David.Apimerika@treefish.co.nz>
2023-10-11 10:52:02 +1300

    Have users update only their own Profile!

--- userauth/urls.py
+++ userauth/urls.py
@@ -3,8 +3,8 @@ from .views import profile_view, CustomUserDeleteView, CustomUserUpdateView
 
 urlpatterns = [
     path('profile/', profile_view, name='account_profile'),
-#    path('profile/update/', CustomUserUpdateView.as_view(template_name='account/update.html'), name='account_update'),
-    path('profile/<int:pk>/update/', CustomUserUpdateView.as_view(template_name='account/update.html'), name='account_update'),
+    path('profile/update/', CustomUserUpdateView.as_view(template_name='account/update.html'), name='account_update'),
+#    path('profile/<int:pk>/update/', CustomUserUpdateView.as_view(template_name='account/update.html'), name='account_update'),
 
 #    path('<int:pk>/delete/', CustomUserDeleteView.as_view(template_name='account/delete.html'), name='account_delete'),
 ]
