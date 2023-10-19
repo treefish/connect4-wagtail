9c8aa2191a0a2b4eb291d9b1c48c5467b93f16f5
David Apimerika <David.Apimerika@treefish.co.nz>
2023-10-19 14:15:27 +1300

    Tidy up for production test

--- userauth/forms.py
+++ userauth/forms.py
@@ -20,18 +20,20 @@ class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=30, label=_("First name"))
     last_name = forms.CharField(max_length=30, label=_("Last name"))
     family_name = forms.CharField(max_length=30, label=_("Family name"), help_text=_("e.g. The Addams Family"))
-    photo_consent = forms.CharField(max_length=30, label=_("Photo consent?"), help_text=_("Allow photography of family for promotional use?"))
+#    photo_consent = forms.BooleanField(label=_("Photo consent?"), help_text=_("Allow photography of family for promotional use?"))
     post_code = forms.CharField(max_length=30, label=_("Post Code"), help_text=_("e.g. PO3 5HL"))
     city = forms.CharField(max_length=30, label=_("City"), help_text=_("e.g. Havant"))
     contact_number = forms.CharField(max_length=30, label=_("Contact Number"), help_text=_("e.g. +615555555"))
-    additional_information = forms.CharField(max_length=30, label=_("Additional information"), help_text=_("Information of importance for event staff to know about your family."))
+    additional_information = forms.CharField(max_length=30, required=False, label=_("Additional information"), help_text=_("Information of importance for event staff to know about your family."))
 
+    # CHOICES = [(True, "Yes"), (False, "No")]
+    # widgets = {'photo_consent': forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)}
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.family_name = self.cleaned_data['family_name']
-        user.photo_consent = self.cleaned_data['photo_consent']
+#        user.photo_consent = self.cleaned_data['photo_consent']
         user.post_code = self.cleaned_data['post_code']
         user.city = self.cleaned_data['city']
         user.contact_number = self.cleaned_data['contact_number']
@@ -44,5 +46,5 @@ class CustomUserUpdateForm(ModelForm):
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'family_name', 'photo_consent', 'post_code', 'city', 'contact_number', 'additional_information',]
 # TODO: Get a widget for True/False for Photo Consent
-        CHOICES = [(True, "Yes"), (False, "No")]
-        widgets = {'photo_consent': forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)}
+#         CHOICES = [(True, "Yes"), (False, "No")]
+#         widgets = {'photo_consent': forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)}
