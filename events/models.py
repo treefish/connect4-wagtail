8a8bfe1cd77d9bd5c9255c9d40257d541021c4a9
David Apimerika <David.Apimerika@treefish.co.nz>
2023-11-14 21:44:30 +1300

    Added Detail field to EventPage, as a RichText field.

--- events/models.py
+++ events/models.py
@@ -3,8 +3,11 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks as wagtail_blocks
 from wagtail.admin.panels import FieldPanel, FieldRowPanel
 
+from streams import blocks
 
 ########################################################################################################################
 #
@@ -108,6 +111,27 @@ class EventPage(Page):
     template = "events/event_page.html"
 
     description = models.TextField(blank=True, max_length=500)
+    detail = StreamField([
+        # ("title", blocks.TitleBlock()),
+        # ("cards", blocks.CardsBlock()),
+        ("image_and_text", blocks.ImageAndTextBlock()),
+        ("cta", blocks.CallToActionBlock()),
+        # ("testimonial", SnippetChooserBlock(
+        #     target_model="testimonials.Testimonial",
+        #     template="streams/testimonial_block.html"
+        # )),
+        #("pricing_table", blocks.PricingTableBlock(table_options=NEW_TABLE_OPTIONS)),
+        # ("richtext_with_title", blocks.RichTextWithTitleBlock()),
+        ("richtext", wagtail_blocks.RichTextBlock(
+            template="streams/event_richtext_block.html",
+            features=["bold", "italic", "ol", "ul", "link", "document-link"])
+         ),
+        # ("large_image", ImageChooserBlock(
+        #     help_text="Ths image will be cropped to 1200px by 775px.",
+        #     template="streams/large_image_block.html"
+        # ))
+    ], null=True, blank=True, use_json_field=True)
+
     internal_page = models.ForeignKey(
         "wagtailcore.Page",
         blank=True,
@@ -145,6 +169,7 @@ class EventPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("description"),
+        FieldPanel("detail"),
         FieldPanel("internal_page"),
         FieldPanel("external_page"),
         FieldPanel("button_text"),
