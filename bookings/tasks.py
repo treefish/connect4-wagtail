1c0eb71a59d8aadc3fee386182baa556c0429d8a
David Apimerika <David.Apimerika@treefish.co.nz>
2023-11-21 15:27:39 +1300

    Adjusting event confirmation e-mail message.

--- bookings/tasks.py
+++ bookings/tasks.py
@@ -20,30 +20,32 @@ def send_booking_email_task(id):
     attendee_names = ", ".join(names)
 
     recipients = [user.email]
-    bcc = ["wagtail@treefish.co.nz"]
-    subject = "Connect4Families Event Booking"
+    bcc = ["connect4@treefish.co.nz"]
+    subject = "Connect4Families Event Booking - {event.title}"
     body = f'''
 Hello {user.first_name},
 
-You have successfully booked for the following Connect4Families event: 
+Thank you for your booking, please save the date in your calendar. The full details of your booking are shown below:
 
-  Event: {event}
+  Event: {event.title}
   Date.: {event.start_date.strftime("%d/%m/%Y")}
   Time.: {event.start_date.strftime("%H:%M %p")} - {event.end_date.strftime("%H:%M %p")}
 
-The following people are booked to come to this event:
+The following people in your group are booked to come to this event:
 
   {attendee_names}
 
 Please arrive in time to complete checking-in and prepare for the event. If you cannot make it to the event, please cancel as soon as possible on-line to allow other families to book and attend.
 
-We look forward to seeing you.
+Note: We reserve the right to cancel or introduce new guidelines resulting from Government notifications.
 
-Regards
-Connect4Familes team
+We look forward to welcoming you.
+
+Best wishes
+Connect4Families Team
 '''
 
-    email = EmailMessage(subject, body, "wagtail@treefish.co.nz", recipients, bcc)
+    email = EmailMessage(subject, body, "connect4@treefish.co.nz", recipients, bcc)
     #    reply_to=["another@example.com"],
     #    headers={"Message-ID": "foo"},
     email.send()
