fc8fb84371b8f4e8fa11177e93257578bc124ff3
David Apimerika <David.Apimerika@treefish.co.nz>
2023-11-24 17:22:22 +1300

    Adding event reminder e-mail messages task. Not yet finished or tested.

--- bookings/tasks.py
+++ bookings/tasks.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from django.utils.timezone import localdate, localtime
+from django.utils import timezone
 
 from celery import shared_task
 from django.core.mail import EmailMessage
@@ -92,3 +94,50 @@ Connect4Families Team
 
     # Delete the booking here because we have to wait for the e-mail to be sent.
     booking.delete()
+
+
+@shared_task()
+def send_event_reminder_email_task(days_ahead=1):
+    print("* Sending e-mail message using send_event_reminder_email_task()")
+    # TODO: Insert here any tests for 'days_ahead' being a whole number.
+    days_ahead_text = "1 day" if days_ahead == 1 else f"{days_ahead} days"
+    all_bookings = Booking.objects.filter(event__start_date__gte=timezone.now()+timedelta(days=days_ahead - 1)).filter(event__start_date__lte=timezone.now()+timedelta(days=days_ahead))
+    for booking in all_bookings:
+        #booking = Booking.objects.get(id=booking_id)
+        event = booking.event
+        user = booking.family
+        attendees = booking.attendance_set.all()
+        names = set(f"{attendee.family_member.first_name} {attendee.family_member.last_name}" for attendee in attendees)
+        attendee_names = ", ".join(names)
+        print(f"* Reminder: {event.title}\t{event.start_date}\t{user.email}\t{attendee_names}")
+        recipients = [user.email]
+        bcc = ["connect4@treefish.co.nz"]
+        subject = f"Connect4Families Event Reminder - {event.title}"
+        body = f'''
+Hello {user.first_name},
+
+This is a reminder that your booked event is now {days_ahead_text} away. The full details of your booking are shown below:
+
+  Family: {user.family_name}
+  Event.: {event.title}
+  Date..: {localdate(event.start_date).strftime("%d/%m/%Y")}
+  Time..: {localtime(event.start_date).strftime("%H:%M %p")} - {localtime(event.end_date).strftime("%H:%M %p")}
+
+The following people in your group are booked to come to this event:
+
+  {attendee_names}
+
+Please arrive in time to complete checking-in and prepare for the event. If you cannot make it to the event, please cancel as soon as possible on-line to allow other families to book and attend.
+
+Note: We reserve the right to cancel or introduce new guidelines resulting from Government notifications.
+
+We look forward to welcoming you.
+
+Best wishes
+Connect4Families Team
+'''
+
+        email = EmailMessage(subject, body, "connect4@treefish.co.nz", recipients, bcc)
+        #    reply_to=["another@example.com"],
+        #    headers={"Message-ID": "foo"},
+        email.send()
